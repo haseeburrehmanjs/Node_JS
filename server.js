3fdfc1b02c318970424d448ca1f98b8921099c49
haseeburrehmanjs <kamranking786500@gmail.com>
2025-03-07 07:04:26 +0500

    create practice folder

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-console.log('hello, i am currently learning backend by (younus bhai)');
+// console.log('hello, i am currently learning backend by (younus bhai)');
 
 
-for(let i = 0, j = 10; i < j; i++){
-    console.log(`haseeb ur rehman` + i);
-}
+// for(let i = 0, j = 10; i < j; i++){
+//     console.log(`haseeb ur rehman` + i);
+// }
+
+
+// node js ak open source, cross platform, back-end runtime environment hai jo javascript code ko run krne k liye use hota hai.
+
+var slug = require('slugify');
+
+const a = slug('some string');
+console.log(a);
